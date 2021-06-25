2bcac379039fe43ebfd34d33e51c67d763b41b10
dpessanha <daopessanha@gmail.com>
2021-06-25 16:55:53 -0300

    aula08

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, ListGroup, Card, Button, Image } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id)
+
   return (
     <div>
-      Product
+      <Link className='btn btn-dark my-3' to='/'>
+        Home
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Valor: R${product.price}</ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
     </div>
   )
 }
